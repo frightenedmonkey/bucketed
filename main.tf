973907879161d0dcccf158438de408629b58606d
Michael Lewis <frightenedmonkey@gmail.com>
2018-10-21 21:39:23 -0700

    Update the bucket with a policy that actually lets things read

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@
 resource "aws_s3_bucket" "main" {
   bucket = "${var.domain}"
   acl    = "public-read"
+  policy = "${data.aws_iam_policy_document.main.json}"
 
   tags {
     "Name" = "${var.domain}"
@@ -15,6 +16,22 @@ resource "aws_s3_bucket" "main" {
   website {
     index_document = "index.html"
     error_document = "error.html"
+  }
+}
+
+data "aws_iam_policy_document" "main" {
+  statement {
+    sid = "publicWebsiteBucket"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "arn:aws:s3:::${var.domain}/*",
+    ]
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+    }
   }
 }
 
