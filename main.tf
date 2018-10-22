8fb4faedf6a30af05243b7e813ba4ee3a5a58e65
Michael Lewis <frightenedmonkey@gmail.com>
2018-10-21 19:50:04 -0700

    Fix bad reference to the route53 validation record
    The aws_acm_certificate_validation resource was referencing the main
    route53 record that was set up to point to the cloudfront distribution.
    That caused the validation resource to consistently fail on apply. This
    change fixes that reference.

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_acm_certificate_validation" "main" {
   certificate_arn = "${aws_acm_certificate.main.arn}"
 
   validation_record_fqdns = [
-    "${aws_route53_record.main.fqdn}"
+    "${aws_route53_record.validation.fqdn}"
   ]
 }
 
